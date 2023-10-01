2f7cf65bfa0ef32ed05a88491f711e79d3c33081
Ming <mingweichan1@gmail.com>
2023-10-01 11:08:05 +0900

    changed Ming to Ming Chan

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi everyone I'm Ming. This is my first Next js website and lovely to meet you all!!!!!</p>
+        <p>Hi everyone I'm Ming Chan. This is my first Next js website and lovely to meet you all!!!!!</p>
         <p>
           This is a sample website. You'll be building a site like this on 
           <a href="https://nextjs.org/learn"> our Next.js tutorial</a>
